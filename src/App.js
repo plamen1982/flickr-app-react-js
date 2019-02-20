6c37c9a678f1c00043b3d079072c133413679f95
“[plamen1982]” <plamen.hristov@ymail.com>
2019-02-20 11:04:30 +0200

    added three column grid system from semantic ui library

--- src/App.js
+++ src/App.js
@@ -1,36 +1,66 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const url = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1';
+const url =
+    "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1";
 
 class App extends Component {
     constructor(props) {
-      super(props)
+        super(props);
 
-      this.state = {
-        items: []
-      }
+        this.state = {
+            items: []
+        };
     }
     componentDidMount() {
-      fetch(url)
-        .then(respose => {
-            if(respose.ok) {
-              return respose.json();
-            } else {
-              console.log('something whent wrong')
-            }
-        })
-        .then(data => {
-          const { items } = data;
-          this.setState({ items })
-        })
-        .catch((e) => {
-          console.log(e)
-        })
+        fetch(url)
+            .then(respose => {
+                if (respose.ok) {
+                    return respose.json();
+                } else {
+                    console.log("something whent wrong");
+                }
+            })
+            .then(data => {
+                const { items } = data;
+                this.setState({ items });
+            })
+            .catch(e => {
+                console.log(e);
+            });
     }
     render() {
+        const { items } = this.state;
         return (
-          <div className="ui container">Flickr App</div>
+
+            
+            <div className="ui celled grid">
+              <div className="row">
+
+                {items.length === 0 ? (
+                    <div>...Loading</div>
+                ) : (
+                    items.map(item => (
+                        <div className="three wide column">
+                            <img
+                                alt="nice pic"
+                                src={item.media.m}
+                                className="ui image"
+                            />
+                            <div className="content">
+                                <div className="header">{item.author}</div>
+                                <div className="meta">
+                                    <span className="date">{item.date_taken}</span>
+                                </div>
+                            </div>
+                            <div className="extra content">
+                                <div className="ui tags"> {item.tags} </div>
+                            </div>
+                        </div>
+                    ))
+                )}
+              </div>
+            </div>
         );
     }
 }
